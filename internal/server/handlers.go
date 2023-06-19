0ca7ff451fa80ec57cb060fdbf9e7eb9be3f8e4b
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-06-19 13:26:04 -0400

    Update workflows; Removed unnecessary event processing
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -10,40 +10,6 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-// func (s *Server) processEvent(messages <-chan *message.Message) {
-// 	for msg := range messages {
-// 		s.Logger.Infof("received event message: %s, payload: %s\n", msg.UUID, string(msg.Payload))
-
-// 		m, err := events.UnmarshalEventMessage(msg.Payload)
-// 		if err != nil {
-// 			s.Logger.Errorw("unable to unmarshal event message", "error", err)
-// 			msg.Nack()
-// 		}
-
-// 		if slices.ContainsFunc(m.AdditionalSubjectIDs, s.locationCheck) || len(s.Locations) == 0 {
-// 			lb, err := loadbalancer.NewLoadBalancer(m.SubjectID, m.AdditionalSubjectIDs)
-// 			if err != nil {
-// 				s.Logger.Errorw("unable to initialize loadbalancer", "error", err, "messageID", msg.UUID)
-// 				msg.Nack()
-// 			}
-
-// 			if lb.LbType != loadbalancer.TypeNoLB {
-// 				switch {
-// 				case m.EventType == "create" && lb.LbType == loadbalancer.TypeLB:
-// 					s.Logger.Debugw("stub for creating loadbalancer", "loadbalancer", lb.LoadBalancerID.String())
-// 				case m.EventType == "delete" && lb.LbType == loadbalancer.TypeLB:
-// 					s.Logger.Debugw("stub for deleting loadbalancer", "loadbalancer", lb.LoadBalancerID.String())
-// 				default:
-// 					s.Logger.Debugw("stub for updating loadbalancer", "loadbalancer", lb.LoadBalancerID.String())
-// 				}
-// 			}
-// 		}
-// 		// we need to Acknowledge that we received and processed the message,
-// 		// otherwise, it will be resent over and over again.
-// 		msg.Ack()
-// 	}
-// }
-
 func (s *Server) ProcessChange(messages <-chan *message.Message) {
 	for msg := range messages {
 		m, err := events.UnmarshalChangeMessage(msg.Payload)
